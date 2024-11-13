f6fae266b54c96c6b2bbd083b451a5c2c66f92f9
aimericdrk <aimeric.rouyer@epitech.eu>
2024-11-13 16:07:23 +0100

    :sparkles: translating text + handling db selection depending on the node mode ( dev or prod )

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,20 +1,20 @@
 const mongoose = require("mongoose");
 
 
-const MongoDBURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mydb";
+const MongoDBURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/";
 
 mongoose.set("strictQuery", false);
 
 const mainDB = mongoose.createConnection(MongoDBURI, {
-    dbName: process.env.DB_NAME
+    dbName: process.env.NODE_ENV == "PROD" ? process.env.PROD_DB_NAME : process.env.DEV_DB_NAME,
 });
 
 mainDB.on("error", (err) => {
-    console.error("Erreur de connexion avec la base de données: ", err);
+    console.error("An error occured when trying to connect with the database : ", err);
 });
 
 mainDB.once("open", async () => {
-    console.log("connexion avec la base de données établie");
+    console.log("Connection with database established");
 });
 
 module.exports = { mainDB };
