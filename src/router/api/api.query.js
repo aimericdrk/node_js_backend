7af5024a75de56888e5cf3860d5b985f9c720509
aimericdrk <aimeric.rouyer@epitech.eu>
2024-11-13 16:09:26 +0100

    :recycle: refacto api.js to make it cleaner

--- src/router/api/api.query.js
+++ src/router/api/api.query.js
@@ -1,8 +1,8 @@
 const api_interactions = require("./api.js");
-const get_user_from_session = require("../../middleware/auth.js");
+const checkAuthenticated = require("../../middleware/auth.js");
 
 module.exports = function (app) {
-    app.get("/user_profile_info_api", get_user_from_session, api_interactions.user_profile_info_api);
-    app.get("/navbar_info_api", get_user_from_session, api_interactions.navbar_info_api);
+    app.get("/profile_info", checkAuthenticated, api_interactions.profile);
+    app.get("/profile_picture", checkAuthenticated, api_interactions.getprofilepicture);
 
 };
